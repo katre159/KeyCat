86ac3e96b018a2d69b47207d7d9e2b46b328d41b
Margus Sibrits <margus.sibrits@gmail.com>
2016-10-15 17:33:51 +0300

    Support for fixed size screenshot #27 , also added temporary screenshot saving #15

--- KeyCat/keycat/__main__.py
+++ KeyCat/keycat/__main__.py
@@ -1,23 +1,24 @@
 import sys
-from mouse_events import FullscreenMouseEventCreator, MouseEventListener, MouseClickEventListener
+from mouse_events import FullscreenMouseEventCreator, MouseEventListener, MouseClickEventListener, \
+    FixedSizeScreenshotEventCreator
 from events import EventReceiver
-from screenshot_taker import ScreenshotTaker
+from screen import ScreenshotTaker, ScreenManager
 from keyboard_events import KeyboardEventListener, KeyboardListener, KeyboardStateManager
 
 
 def main(argv):
-
     event_receiver = EventReceiver()
 
     keyboard_event_listener = KeyboardEventListener(KeyboardListener(KeyboardStateManager(event_receiver)))
     keyboard_event_listener.daemon = False
     keyboard_event_listener.start()
 
+    mouse_event_creator = FixedSizeScreenshotEventCreator(ScreenshotTaker(), ScreenManager(), 300, 300)
+
     mouse_click_listener = MouseClickEventListener(
-        MouseEventListener(FullscreenMouseEventCreator(ScreenshotTaker()), event_receiver))
+        MouseEventListener(mouse_event_creator, event_receiver))
     mouse_click_listener.daemon = False
     mouse_click_listener.start()
-
 
 
 if __name__ == '__main__':
--- KeyCat/keycat/events.py
+++ KeyCat/keycat/events.py
@@ -1,8 +1,8 @@
 class EventReceiver(object):
-
     @staticmethod
     def receive_mouse_event(event):
         print("MouseEvent: x = %s, y = %s, screenshot = %s" % (event.click_x, event.click_y, event.screenshot))
+        event.screenshot.save("screenshot_x_" + str(event.click_x) + "_y_" + str(event.click_y) + ".png", "PNG")
 
     @staticmethod
     def receive_keyboard_state_change_event(event):
