e67a3d8b235662556b14329c990a7ac9b29f1909
Katre Metsvahi <katre.metsvahi@gmail.com>
2016-11-16 17:41:25 +0200

    Add fallback options for setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,11 @@ apt-get install python-opencv
 apt-get install python-pip
 
 python setup.py install
+
+# Fallback
+pip install Pillow
+pip install pyscreenshot
+pip install PyUserInput
+pip install screeninfo
+pip install python-xlib
+pip install SQLAlchemy
