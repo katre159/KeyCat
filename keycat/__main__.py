0d8d3c756a700885b821297930794e4c2e5b14d6
hans.miikael <hans.miikael@gmail.com>
2016-11-17 11:11:14 +0200

    Added interrupt signal handling.

--- keycat/__main__.py
+++ keycat/__main__.py
@@ -9,7 +9,13 @@ from button_matcher import ButtonMatcher
 from template_matcher import CCOEFFNORMEDTemplateMatcher
 from keycat.database import *
 from keycat.program_identifier import *
+from time import sleep
+import signal
 
+
+def exit_program(signal, frame):
+    print('Keycat terminated!')
+    sys.exit(0)
 
 def main():
     session = get_database_scoped_session()
@@ -23,7 +29,7 @@ def main():
 
     keyboard_event_listener = KeyboardEventListener(KeyboardListener(
         KeyboardStateManager(event_receiver, program_identifier)))
-    keyboard_event_listener.daemon = False
+    keyboard_event_listener.daemon = True
     keyboard_event_listener.start()
 
     mouse_event_creator = FixedSizeScreenshotEventCreator(ScreenshotTaker(), ScreenManager(), program_identifier,
@@ -31,8 +37,13 @@ def main():
 
     mouse_click_listener = MouseClickEventListener(
         MouseEventListener(mouse_event_creator, event_receiver))
-    mouse_click_listener.daemon = False
+    mouse_click_listener.daemon = True
     mouse_click_listener.start()
+
+    while 1:
+        signal.signal(signal.SIGINT, exit_program)
+        sleep(1)
+
 
 
 if __name__ == '__main__':
